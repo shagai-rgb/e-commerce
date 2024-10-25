a0f6e7ba68bfa7a105d201a181e6abb05c4129a1
shagai-rgb <dash.altanshagai48@gmail.com>
2024-10-25 12:25:35 +0800

    -message "done"

--- back-end/src/routes/imgRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { ImageAdd } from "../../controllers/ImageContoller";
-const imgRoutes = Router();
-
-imgRoutes.post("/images", ImageAdd);
-
-export default imgRoutes;
--- back-end/src/routes/loginRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { loginController } from "../../controllers/loginController";
-
-const loginRouter = Router();
-
-loginRouter.post("/login", loginController);
-
-export default loginRouter;
